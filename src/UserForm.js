8cac70a95e3c73e196bde446a34c87c302090c94
Arkon13 <lordarkon1@gmail.com>
2017-09-25 18:40:39 +0300

    addUserForm

---
+++ src/UserForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class UserForm extends Component {
+  render() {
+    return (
+      <form>
+          <input type="text" placeholder="Name" />
+          <input type="e-mail" placeholder="Email" />
+          <input type="number" placeholder="Tel." />
+          <button type="submit">Submit</button>
+      </form>
+      
+    );
+  }
+}
+
+export default UserForm;
