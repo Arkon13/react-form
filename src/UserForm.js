6e4060c364e4c1d1208a51fb4f2a13e7a8d8dc79
Arkon13 <lordarkon1@gmail.com>
2017-09-25 23:47:05 +0300

    change

--- src/UserForm.js
+++ src/UserForm.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+
+
+
 class UserForm extends Component {
+    
+    constructor (props) {
+        super(props);
+        this.state = {value: ''};
+        
+    }
+
+      componentDidMount() {
+        
+    }
+    handleChange(event) {
+        this.setState({value: event.target.value});
+      }
+
+
+
+
   render() {
     return (
-      <form>
-          <input type="text" placeholder="Name" /> 
-          <input type="e-mail" placeholder="Email" />  
-          <input type="tel" placeholder="Tel." /> 
+      <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="Name" value={this.state.value} onChange={this.handleChange} ref="name"/> 
+          <input type="e-mail" placeholder="Email" value={this.state.value} onChange={this.handleChange} ref="email"/>  
+          <input type="tel" placeholder="Tel." value={this.state.value} onChange={this.handleChange} ref="tel"/> 
           <button type="submit">Submit</button>
       </form>
       
